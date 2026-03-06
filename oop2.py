cd0b0ffff1941769eeca4d397e327f0b39f7b404
Ektawadurkar <ektawadurkar@gmail.com>
2026-03-06 23:59:53 +0530

    oop2.py

---
+++ oop2.py
@@ -0,0 +1,11 @@
+class Student:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+# creating object
+s1 = Student("ekta", 20)
+
+print("Name:", s1.name)
+print("Age:", s1.age)
